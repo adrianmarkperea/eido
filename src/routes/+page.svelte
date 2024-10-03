07b2c6a562bb42527861c27f51844ed84cc7d915
Adrian Perea <adrianmarkperea@gmail.com>
2024-10-03 09:12:11 +0800

    Add support for mdsvex

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h2 class="text-lg font-bold">Example blog post</h2>
-<p class="text-sm text-gray-400">December 13, 2018</p>
+<script lang="ts">
+	import { formatDate } from '$lib/utils';
+
+	export let data;
+</script>
+
+<section>
+	<ul>
+		{#each data.posts as post}
+			<li class="mt-10">
+				<h2 class="text-xl font-bold">{post.title}</h2>
+				<p class="text-sm text-gray-500">{formatDate(post.date)}</p>
+				<p>{post.description}</p>
+			</li>
+		{/each}
+	</ul>
+</section>
