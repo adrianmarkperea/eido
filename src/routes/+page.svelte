8095ca890e2686857b3027b77786c12de1fe92f9
adrianmarperea <adrianmarkperea@gmail.com>
2024-10-06 18:04:29 +0800

    Remove useless element

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,16 +9,14 @@
 	<title>{config.title}</title>
 </svelte:head>
 
-<section>
-	<ul>
-		{#each data.posts as post}
-			<a href={post.slug}>
-				<li class="mt-16">
-					<h3 class="text-3xl font-black">{post.title}</h3>
-					<p class="text-sm text-gray-500 font-serif mt-1">{formatDate(post.date)}</p>
-					<p class="font-serif mt-2">{post.peep}</p>
-				</li>
-			</a>
-		{/each}
-	</ul>
-</section>
+<ul>
+	{#each data.posts as post}
+		<a href={post.slug}>
+			<li class="mt-16">
+				<h3 class="text-3xl font-black">{post.title}</h3>
+				<p class="text-sm text-gray-500 font-serif mt-1">{formatDate(post.date)}</p>
+				<p class="font-serif mt-2">{post.peep}</p>
+			</li>
+		</a>
+	{/each}
+</ul>
